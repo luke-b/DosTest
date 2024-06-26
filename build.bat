5151b613df44b4dc59181ef43466e8f439852395
luke-b <luke.benda@gmail.com>
2024-06-26 14:40:50 +0200

    updated bat file

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 REM Set up the environment for Borland C++
 SET PATH=c:\BORLANDC\BIN;%PATH%
@@ -8,8 +9,16 @@ SET LIB=c:\BORLANDC\LIB
 REM Change to the source directory
 cd \WOLFSRC
 
-REM Compile the project
-bcc @wolf3d.prj
+REM Compile the project and log output
+echo Compiling Wolf3D...
+bcc @wolf3d.prj > c:\WOLF3D\build_log.txt 2>&1
+
+REM Check if compilation was successful
+if %errorlevel% neq 0 (
+    echo Compilation failed. Check build_log.txt for details.
+    type c:\WOLF3D\build_log.txt
+    exit /b %errorlevel%
+)
 
 REM Copy the compiled executable and required files to the output directory
 xcopy \WOLFSRC\*.exe \WOLF3D /Y
